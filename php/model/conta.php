331baa3f36878b211a2eeded3eab4123610815ab
Pedro Teixeira <pedroteixeirabisognin@gmail.com>
2017-09-26 01:11:27 -0300

    model errado, testando cadastro conta no banco

--- php/model/conta.php
+++ php/model/conta.php
@@ -12,4 +12,24 @@
 
 
 
+$link = mysqli_connect("localhost", "root", "", "tcc");
+ 
+if (!$link) {
+    echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+ 
+echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
+
+$sql = "INSERT INTO conta VALUES ";
+$sql .= "(1,1,'$login', '$senha', '$email')"; 
+mysqli_query($link,$sql) or die("Erro ao tentar cadastrar registro");
+mysqli_close($link);
+echo "Cliente cadastrado com sucesso!";
+mysqli_close($link);
+
+
+
 ?>
